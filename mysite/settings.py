4928dde2d469e79ea840011d28411ca400486918
Shyju <shyju.ak@gmail.com>
2017-01-03 14:00:38 +0530

    added pysolution

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    #'blog',
+    'pysolution',
 ]
 
 MIDDLEWARE_CLASSES = [
